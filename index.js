fc06c60312b71d068a7384ea0706b8c6a647fca9
Kenjie Decavero <kenjiedec@gmail.com>
2020-06-09 11:08:27 +0700

    Update 1.0.1

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const capitalizeFirstLetter = (thetext) => {
-    return string.charAt(0).toUpperCase() + thetext.slice(1);
+    return thetext.charAt(0).toUpperCase() + thetext.slice(1);
 }
 const unCapitalizeFirstLetter = (thetext) => {
-    return string.charAt(0).toLowerCase() + thetext.slice(1);
+    return thetext.charAt(0).toLowerCase() + thetext.slice(1);
 }
 const CapitalizeFirst = (thetext) => {
-    return string.charAt(0).toUpperCase() + thetext.slice(1);
+    return thetext.charAt(0).toUpperCase() + thetext.slice(1);
 }
 const unCapitalizeFirst = (thetext) => {
-    return string.charAt(0).toLowerCase() + thetext.slice(1);
+    return thetext.charAt(0).toLowerCase() + thetext.slice(1);
 }
 const cfl = (thetext) => {
-    return string.charAt(0).toUpperCase() + thetext.slice(1);
+    return thetext.charAt(0).toUpperCase() + thetext.slice(1);
 }
 const ucfl = (thetext) => {
-    return string.charAt(0).toLowerCase() + thetext.slice(1);
+    return thetext.charAt(0).toLowerCase() + thetext.slice(1);
 }
 const cFL = (thetext) => {
-    return string.charAt(0).toUpperCase() + thetext.slice(1);
+    return thetext.charAt(0).toUpperCase() + thetext.slice(1);
 }
 const uCFL = (thetext) => {
-    return string.charAt(0).toLowerCase() + thetext.slice(1);
+    return thetext.charAt(0).toLowerCase() + thetext.slice(1);
 }
 module.exports = {
     capitalizeFirstLetter,
